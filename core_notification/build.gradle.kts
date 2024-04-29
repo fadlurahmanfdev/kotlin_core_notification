915f9af9d4659269a40300d87f824f33febb81b3
fadlurahmanf <tffajari@gmail.com>
2024-04-29 21:20:03 +0700

    feat(example): update version

--- core_notification/build.gradle.kts
+++ core_notification/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         register<MavenPublication>("release"){
             groupId = "com.github.fadlurahmanfdev"
             artifactId = "core_notification"
-            version = "0.0.1"
+            version = "0.0.2"
 
             afterEvaluate {
                 from(components["release"])
