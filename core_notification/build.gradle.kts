18851ed7ec617ceac336708529a52c7caa13ec77
fadlurahmanf <tffajari@gmail.com>
2024-04-29 21:27:34 +0700

    feat(version): increase code version

--- core_notification/build.gradle.kts
+++ core_notification/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         register<MavenPublication>("release"){
             groupId = "com.github.fadlurahmanfdev"
             artifactId = "kotlin_core_notification"
-            version = "0.0.2"
+            version = "0.0.3"
 
             afterEvaluate {
                 from(components["release"])
